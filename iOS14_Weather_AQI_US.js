2fb5805a43d0e3dbf732ea56093356358dff123c
Wist <62319737+zeng-ws@users.noreply.github.com>
2021-06-21 11:37:41 +0800

    master

--- iOS14_Weather_AQI_US.js
+++ iOS14_Weather_AQI_US.js
@@ -2,7 +2,7 @@
 // Adopted to Quantumult X Rewrite Script by laosb (https://lao.sb/gh)
 
 // STEP 1: 前往 https://aqicn.org/data-platform/token/ 注册账户，将申请的 API Token 填入下方
-const aqicnToken = 'a6ff711cf2b69c474b5090f1cd56afb0e2eaef5d'
+const aqicnToken = 'c9ec888228e4c08ec2e8e494d0bab2f423c94e96'
 
 // STEP 2: 参考下方配置片段，在代理工具的配置文件中添加对应的配置。注意：script-response-body 后应该替换为添加 apicnToken 值后的脚本路径
 /*
